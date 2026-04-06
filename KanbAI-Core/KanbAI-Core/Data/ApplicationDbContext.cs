a89ae874aea0c07f56a81c049571ee816cb9a94e
Gulybán Dániel <gulyban.daniel@gmail.com>
2026-04-06 15:12:45 +0200

    entity framework and local database are configured

---
+++ KanbAI-Core/KanbAI-Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace KanbAI_Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- KanbAI-Core/KanbAI-Core/Program.cs
+++ KanbAI-Core/KanbAI-Core/Program.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.EntityFrameworkCore;
+using KanbAI_Core.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
