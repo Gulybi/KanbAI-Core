96d6a70c9684a8ecaf4b2377bd1ce17d875544c4
Gulybán Dániel <gulyban.daniel@gmail.com>
2026-04-13 18:40:17 +0200

    26 generate initial ef core migration and apply to database

---
+++ KanbAI-Core/KanbAI-Core/Migrations/20260411140745_AddProjectDescription.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanbAI_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Projects");
+        }
+    }
+}
