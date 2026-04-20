e3bf881ca2ff7bb6a342c11a8b86dc10393939e1
Gulybán Dániel <76229075+Gulybi@users.noreply.github.com>
2026-04-20 17:41:13 +0200

    35 configure jwt authentication (#49)
    - Add Microsoft.AspNetCore.Authentication.JwtBearer package
    - Define JwtSettings model and add development configuration
    - Configure JWT bearer authentication and token validation logic in Program.cs
    - Register authorization services
    
    Co-authored-by: Gulybán Dániel <gulyban.daniel@gmail.com>

--- KanbAI-Core/KanbAI-Core/Program.cs
+++ KanbAI-Core/KanbAI-Core/Program.cs
@@ -1,8 +1,39 @@
 using KanbAI_Core.Extensions;
+using System.Text;
+using KanbAI_Core.Models.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var jwtSettings = new JwtSettings();
+builder.Configuration.Bind("JwtSettings", jwtSettings);
+builder.Services.AddSingleton(jwtSettings);
+
 builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = jwtSettings.Issuer,
+            ValidateAudience = true,
+            ValidAudience = jwtSettings.Audience,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
+builder.Services
+    .AddAuthorization()
     .AddPersistence(builder.Configuration)
     .AddApiInfrastructure()
     .AddAuthServices()
